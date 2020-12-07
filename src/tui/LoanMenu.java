9523438fa9cfe2685d5290da1391d422e333d2f7
adidas420x <58811906+adidas420x@users.noreply.github.com>
2020-12-07 11:01:15 +0100

    s

--- src/tui/LoanMenu.java
+++ src/tui/LoanMenu.java
@@ -1,5 +1,49 @@
 package tui;
 
+import controller.LoanController;
+
 public class LoanMenu {
 
+	private LoanController loanController;
+
+	public LoanMenu() {
+		loanController = new LoanController();
+	}
+
+	public void start() {
+		loanMenu();
+	}
+
+	private void loanMenu() {
+		boolean exit = false;
+		while (!exit) {
+			int choice = writeLoanMenu();
+			switch (choice) {
+			case 1:
+				//needs to be implemented VV
+				//createLoan();
+				break;
+			case 2:
+				System.out.println("Feature not yet implemented.");
+				break;
+			case 3:
+				System.out.println("Bye!");
+				exit = true;
+				break;
+			default:
+				System.out.println("Not an option");
+				break;
+			}
+		}
+	}
+
+	private int writeLoanMenu() {
+		int choice = 0;
+		System.out.println("\n*** Loan Menu ***");
+		System.out.println(" (1) Create Loan");
+		System.out.println(" (2) Find a loan");
+		System.out.println(" (3) Exit");
+		choice = TextInput.inputNumber("\nPick an option");
+		return choice;
+	}
 }
--- src/tui/MainMenu.java
+++ src/tui/MainMenu.java
@@ -5,5 +5,49 @@ public class MainMenu {
 	private LoanMenu loanMenu;
 	private ClientMenu clientMenu;
 	
+	public MainMenu() {
+		saleMenu = new SaleMenu();
+		loanMenu = new LoanMenu();
+		clientMenu = new ClientMenu();
+	}
 	
+	public void start() {
+		mainMenu();
+	}
+	
+	private void mainMenu() {
+		boolean exit = false;
+		while (!exit) {
+			int choice = writeMainMenu();
+			switch (choice) {
+			case 1:
+				saleMenu.start();
+				break;
+			case 2:
+				loanMenu.start();
+				break;
+			case 3:
+				clientMenu.start();
+				break;
+			case 4:
+				System.out.println("Bye!");
+				exit = true;
+				break;
+			default:
+				System.out.println("Not an option");
+				break;
+			}
+		} 
+	}
+	
+	private int writeMainMenu() {
+		int choice = 0;
+		System.out.println("\n*** Main Menu ***");
+		System.out.println(" (1) Sale Menu");
+		System.out.println(" (2) Loan Menu");
+		System.out.println(" (3) Client Menu");
+		System.out.println(" (4) Exit");
+		choice = TextInput.inputNumber("\nPick an option");
+		return choice;
+	}
 }
