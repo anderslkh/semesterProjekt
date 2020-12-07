ea8226bb54dd444398dd3c6e2679d9419f2ec8a8
adidas420x <58811906+adidas420x@users.noreply.github.com>
2020-12-07 13:55:10 +0100

    Update SaleProduct.java
    s

--- src/model/SaleProduct.java
+++ src/model/SaleProduct.java
@@ -1,5 +1,58 @@
 package model;
 
 public class SaleProduct {
+	private String saleProductName;
+	private String madeByCompany;
+	private String description;
+	private int saleItemNumber;
+	private String saleBarcode;
+
+	public SaleProduct(String saleProductName, String madeByCompany, String description, int saleItemNumber, String saleBarcode) {
+		this.saleProductName = saleProductName;
+		this.madeByCompany = madeByCompany;
+		this.description = description;
+		this.saleItemNumber = saleItemNumber;
+		this.saleBarcode = saleBarcode;
+	}
+
+	public String getSaleProductName() {
+		return saleProductName;
+	}
+
+	public void setSaleProductName(String saleProductName) {
+		this.saleProductName = saleProductName;
+	}
+
+	public String getMadeByCompany() {
+		return madeByCompany;
+	}
+
+	public void setMadeByCompany(String madeByCompany) {
+		this.madeByCompany = madeByCompany;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getSaleItemNumber() {
+		return saleItemNumber;
+	}
+
+	public void setSaleItemNumber(int saleItemNumber) {
+		this.saleItemNumber = saleItemNumber;
+	}
+
+	public String getSaleBarcode() {
+		return saleBarcode;
+	}
+
+	public void setSaleBarcode(String saleBarcode) {
+		this.saleBarcode = saleBarcode;
+	}
 
 }
