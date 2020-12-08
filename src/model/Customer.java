eb9b6b3fd4d67781b17775a5e86d78e33d66f972
BigGreen98 <75620519+BigGreen98@users.noreply.github.com>
2020-12-08 12:14:18 +0100

    jaja

--- src/model/Customer.java
+++ src/model/Customer.java
@@ -1,5 +1,82 @@
 package model;
 
 public class Customer {
+	private int customerID;
+	private String name;
+	private String phone;
+	private String mail;
+	private String address;
+	private String zipCode;
+	private String city;
+	
+	public Customer(int customerID, String name, String phone, String mail, String address, String zipCode,
+			String city) {
+		this.customerID = customerID;
+		this.name = name;
+		this.phone = phone;
+		this.mail = mail;
+		this.address = address;
+		this.zipCode = zipCode;
+		this.city = city;
+	}
 
+	public int getCustomerID() {
+		return customerID;
+	}
+
+	public void setCustomerID(int customerID) {
+		this.customerID = customerID;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getZipCode() {
+		return zipCode;
+	}
+
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+	
+	
+	
 }
--- src/model/CustomerContainer.java
+++ src/model/CustomerContainer.java
@@ -1,5 +1,10 @@
 package model;
 
-public class CustomerContainer {
+import java.util.ArrayList;
 
+public class CustomerContainer {
+	private ArrayList<Customer> customers;
+	private static CustomerContainer instance;
+	
+	public CustomerContainer()
 }
