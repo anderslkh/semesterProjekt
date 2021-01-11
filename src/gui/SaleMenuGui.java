24d2609829d4c861c7583b33dbac1a28c7140620
adidas420x <58811906+adidas420x@users.noreply.github.com>
2021-01-11 10:33:03 +0100

    Update SaleMenuGui.java

--- src/gui/SaleMenuGui.java
+++ src/gui/SaleMenuGui.java
@@ -9,10 +9,16 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
 import java.awt.FlowLayout;
 import javax.swing.JList;
+import javax.swing.JTextPane;
+import javax.swing.JTextField;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
 
 public class SaleMenuGui extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField vareNrTextField;
+	private JTextField txtIndtastAntal;
 
 	/**
 	 * Launch the application.
@@ -60,6 +66,37 @@ public class SaleMenuGui extends JFrame {
 		
 		JButton udlånButton = new JButton("Udl\u00E5n");
 		panel.add(udlånButton);
+		
+		JPanel panel_1 = new JPanel();
+		FlowLayout flowLayout_1 = (FlowLayout) panel_1.getLayout();
+		flowLayout_1.setAlignment(FlowLayout.LEFT);
+		contentPane.add(panel_1, BorderLayout.WEST);
+		
+		JTextPane vareTxtpn = new JTextPane();
+		vareTxtpn.setText("Vare");
+		panel_1.add(vareTxtpn);
+		
+		vareNrTextField = new JTextField();
+		vareNrTextField.setText("Indtast vare nr.");
+		panel_1.add(vareNrTextField);
+		vareNrTextField.setColumns(10);
+		
+		JTextPane længdeTxtpn = new JTextPane();
+		længdeTxtpn.setText("L\u00E6ngde");
+		panel_1.add(længdeTxtpn);
+		
+		JComboBox længdeComboBox = new JComboBox();
+		længdeComboBox.setModel(new DefaultComboBoxModel(new String[] {"1200mm", "2400mm", "3200mm", "3600mm", "4200mm"}));
+		panel_1.add(længdeComboBox);
+		
+		JTextPane antalTxtpn = new JTextPane();
+		antalTxtpn.setText("Antal");
+		panel_1.add(antalTxtpn);
+		
+		txtIndtastAntal = new JTextField();
+		txtIndtastAntal.setText("Indtast antal");
+		panel_1.add(txtIndtastAntal);
+		txtIndtastAntal.setColumns(10);
 	}
 
 }
