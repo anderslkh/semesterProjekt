289013ac4ec329969ad77e0b43c2ceb4fb41d389
HyggenHD <75610123+HyggenHD@users.noreply.github.com>
2021-01-20 11:40:28 +0100

    HEJ :)

--- src/gui/TryMe.java
+++ src/gui/TryMe.java
@@ -13,7 +13,7 @@ public class TryMe {
 		saleProductContainer.addSaleProduct(new SaleProduct("Hammer", 122, "Hammer A/S", "Super god hammer, tager det hele", "0001335472859", 99, "11-12-20"));
 		saleProductContainer.addSaleProduct(new SaleProduct("Skruer", 121, "Skruer A/S", "Super gode skruer, passer til det meste", "0001135376159", 75, "11-12-20"));
 		
-		MainMenu SaleMenuGui = new MainMenu();
-		SaleMenuGui.start();
+		SaleMenuGui JFrameInit = new SaleMenuGui();
+		JFrameInit.setVisible(true);
 	}
 }
